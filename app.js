11ddaea1be31c37b353a1bdb652e2b7e925afc2c
mabvmex <mabvmex@gmail.com>
2018-07-19 14:54:04 -0500

    Jueves

--- app.js
+++ app.js
@@ -37,8 +37,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Express View engine setup
 
+
+// Express View engine setup
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+
 const passport = require('passport');
 const sendActivationLink = require('../helpers/mailer').sendActivationLink;
 const passportFacebook = require('../helpers/facebook');
+
+const User = require('../models/User');
+const Photo = require('../models/Photo');
+const Event = require('../models/Event');
+const Music = require('../models/Music');
+
 
 const errDict = {
   UserExistsError: 'Este usuario ya existe'
@@ -19,24 +25,23 @@ function isLoggedIn(req, res, next) {
   return res.redirect('/login');
 }
 
+// router.get('/layout', (req, res)=>{ 
+//   console.log(req.user)
+//   res.render('layout', req.user) 
+//})
 
-router.get('/activation', isLoggedIn, (req, res, next) => {
-  User.findByIdAndUpdate(req.user._id, {active: true}, {new: true})
-  .then(user=>{
-    res.send('Activado, gracias ' +  user.username);
-  })
-  .catch(e=>next(e))
-})
 
+//logout
 router.get('/logout', isLoggedIn, (req, res, next) => {
   req.logout();
   res.redirect('/login');
 })
 
+
+//SignUp
 router.get('/signup', (req, res, next) => {
   res.render('auth/signup');
 });
-
 router.post('/signup', (req, res, next) => {
   if(req.body.password !== req.body.repeatpassword) {
     req.body.err = 'Tu password no coincide';
@@ -54,6 +59,14 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
+//activaciónLink
+router.get('/activation', isLoggedIn, (req, res, next) => {
+  User.findByIdAndUpdate(req.user._id, {active: true}, {new: true})
+  .then(user=>{
+    res.send('Activado, gracias ' +  user.username);
+  })
+  .catch(e=>next(e))
+})
 
 //loginFacebook
 router.get('/facebook', passportFacebook.authenticate('facebook'));
@@ -73,11 +86,123 @@ router.post('/login', passport.authenticate('local'), (req, res, next) => {
   res.redirect('/profile');
 });
 
-
-
+//Logout
 router.post('/logout',  (req, res, next) => {
   req.logout();
   res.redirect('/login');
+});
+
+
+/* GET Photo Gallery */
+router.get('/gallery', (req, res, next) => {
+  Photo.find({}, (err, photos) => {
+    if(err) {
+      console.log(err); 
+    } else {
+      res.render('gallery', {photos});
+    }
+  });
+})
+
+/* GET New Photo form */
+router.get('/gallery/new-photo', (req, res, next) => {
+  res.render('new-photo');
+})
+
+/* GET Create new Photo*/
+router.post('/gallery/new-photo', (req, res, next) => {
+  Photo.create(req.body)
+  .then(
+    res.redirect('/gallery')
+  )
+  .catch(e=>next(e))
+});
+
+/*  DELETE Photo */
+router.delete('/gallery/delete/:id', (req, res, next) => {
+  console.log(req.params.id);
+  Photo.findByIdAndRemove(req.params.id)
+  .then(item=>{
+    console.log("borrado papi", item)
+    res.status(200).json(item);
+  })
+  .catch(e=>next(e))
+});
+
+
+
+/* GET Music */
+router.get('/music', (req, res, next) => {
+  Music.find({}, (err, musics) => {
+    if(err) {
+      console.log(err); 
+    } else {
+      res.render('music', {musics});
+    }
+  });
+})
+
+/* GET New Music form */
+router.get('/music/new-music', (req, res, next) => {
+  res.render('new-music');
+})
+
+/* GET Create new music */
+router.post('/music/new-music', (req, res, next) => {
+  Music.create(req.body)
+  .then(
+    res.redirect('/music')
+  )
+  .catch(e=>next(e))
+})
+
+
+
+/* GET Photo Gallery */
+router.get('/gallery', (req, res, next) => {
+  Photo.find({}, (err, photos) => {
+    if(err) { 
+    } else {
+      res.render('gallery', {photos});
+    }
+  });
+})
+
+
+
+/* GET Events */
+router.get('/events', (req, res, next) => {
+  Event.find({}, (err, eventos)=>{
+    res.render('events', eventos);
+   // console.log(" qui van eventos",eventos);
+  })
+})
+
+/* GET New event form */
+router.get('/events/new-events', (req, res, next) => {
+  res.render('new-events');
+});
+
+/* POST Create new event*/
+router.post('/events/new-events', (req, res, next) => {
+  Event.create(req.body)
+  .then(
+    res.redirect('/events')
+  )
+  .catch(e=>next(e))
+});
+
+/* GET Events-details*/
+router.get('/events/events-details/:id', (req, res, next) => {
+  Event.findById(req.params.id,(e,ev)=>{
+      console.log("ENTRAMOS AL THEN " + ev)
+      res.render('events-details',ev);
+  })
+});
+  router.post('/events/events-details/', (req, res, next) => {
+    Event.find({}, (err, detalles)=>{
+        res.redirect('/events-details/', detalles)
+    })
 });
 
 
